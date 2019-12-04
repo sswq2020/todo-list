e757c7b6d756f58374467ed2531b7898b51e46bc
shiswq@hletong.com <shiswq@hletong.com>
2019-12-04 15:56:35 +0800

    feat: 🎸 在app.module.ts里引入RouterModule,Routes构建路由的必要类和接口

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { AboutComponent } from './about/about.component';
 import { NavComponent } from './layout/nav/nav.component';
+// import { AppRoutingModule } from './app-routing.module';
+
+const appRoutes: Routes = [
+  { path: 'todo', component: TodoComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '', redirectTo: '/todo', pathMatch: 'full'}
+];
 
 // 装饰器@NgModule,用来装饰AppModule类
 @NgModule({
@@ -17,7 +25,8 @@ import { NavComponent } from './layout/nav/nav.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes)
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent] // 项目启动从AppComponent开始
