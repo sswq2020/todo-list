902b581ce68d701d5ccbcfbb897645a1259b7b85
shiswq@hletong.com <shiswq@hletong.com>
2019-12-27 09:04:43 +0800

    feat: 🎸 增加详情页的动态路由,与Vue-Router几乎一样

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { TodoComponent } from './todo/todo.component';
+import { DetailComponent } from './detail/detail.component';
 
 const appRoutes: Routes = [
   { path: 'todo', component: TodoComponent},
+  { path: 'todo/:id', component: DetailComponent},
   { path: 'about', loadChildren: './about/about.module#AboutModule'},
   { path: '', redirectTo: '/todo', pathMatch: 'full'}
 ];
