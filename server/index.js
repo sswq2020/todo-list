2eb12a6ba15c87038c77e74277415fd0b826cf40
shiswq@hletong.com <shiswq@hletong.com>
2020-02-06 08:42:08 +0800

    feat: 🎸 增加简单的后端服务

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const multer = require('multer');
+const upload = multer({dest:'upload'})
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.get('/api/v1/people',(req,res)=>{
+  res.json([{"name":"张三","age":12},{"name":"李四","age":22}])
+})
+
+app.post('/api/v1/people',(req,res)=>{
+  res.json(req.body)
+})
+
+app.post('/api/v1/people/123/avatar',upload.single('avatar'),(req,res)=>{
+  res.send(res.file)
+})
+
+app.listen(3000)
+
+console.log("Server is runninf at port 3000")
