7c9c31fcae3e8a8089b58d67c06c35e2d9ddc5c8
shiswq@hletong.com <shiswq@hletong.com>
2020-02-08 15:18:10 +0800

    feat: 🎸 增加3种包含上传的处理逻辑

--- src/app/providers/people.service.ts
+++ src/app/providers/people.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse, HttpEvent } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { retry, retryWhen, delay, mergeMap } from 'rxjs/operators';
 
-interface People {
+export interface People {
   name: string;
   age: number;
+  id: number;
 }
 
 
@@ -15,7 +17,42 @@ export class PeopleService {
 
   constructor(private http: HttpClient) {
   }
-  fetchPeople(): Observable<any> {
-    return this.http.get('/api/v1/people');
+
+  uploadFile(data): Observable<HttpEvent<object>> {
+    return this.http.post<HttpEvent<object>>('/api/v1/people/123/avatar', data, {
+      reportProgress: true,
+      observe: 'events'
+    });
   }
+
+
+  fetchPeople(): Observable<HttpResponse<People[]>> {
+    return this.http.get<People[]>('/api/v1/people', { observe: 'response' });
+  }
+
+  // fetchPeople(): Observable<People[]> {
+  //   return this.http.get<People[]>('/api/v1/people', {
+  //     headers: {
+  //       'app-language': 'putonghua1'
+  //     }
+  //   });
+  // }
+
+  // fetchPeople(): Observable<People[]> {
+  //   return this.http.get<People[]>('/api/v2/people').pipe(
+  //     retryWhen(error => {
+  //       let retries = 2;
+  //       return error.pipe(
+  //         delay(1000),
+  //         mergeMap(err => {
+  //           if (retries-- > 0) {
+  //             return of(err);
+  //           } else {
+  //             return throwError(err)
+  //           }
+  //         })
+  //       );
+  //     })
+  //   );
+  // }
 }
