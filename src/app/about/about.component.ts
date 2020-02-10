63665c4334811532eb962ce866b95f5f7edba210
shiswq@hletong.com <shiswq@hletong.com>
2020-02-10 20:40:20 +0800

    feat: 🎸 about组件实现一个定时器

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subject, Observable, merge } from 'rxjs';
+import { map, startWith, scan, mapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'app-about', // 选择器
@@ -8,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 // 在Angular 8中,一个组件就是一个类,使用@Component装饰器进行装饰
 export class AboutComponent implements OnInit {
 
+  clock: Observable<Date>;
+
+  update$ = new Subject();
+
   constructor() { }
 
   ngOnInit() {
+    this.clock = merge(
+      interval(3000).pipe(mapTo('second')),
+      this.update$.asObservable().pipe(mapTo('hour'))
+    ).pipe(
+      startWith('2017/12/22'),
+      scan((acc, cur) => {
+        const date = new Date(acc);
+        if (cur === 'hour') {
+          date.setHours(date.getHours() + 1);
+        } else {
+          date.setSeconds(date.getSeconds() + 1);
+        }
+        return date;
+      })
+    ) as Observable<Date>;
   }
+
 
 }
